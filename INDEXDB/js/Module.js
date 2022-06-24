06529d977006088cc03558ea8d9624fa67000d28
Akinyele Emmanuel <56391374+akinyeleoe@users.noreply.github.com>
2022-06-24 16:43:00 +0400

    Update Module.js

--- INDEXDB/js/Module.js
+++ INDEXDB/js/Module.js
@@ -7,4 +7,32 @@ const productdb =(dbname, table) =>{
     return db;
 }
 
+//INSERT FUNCTION
+const bulkcreate =(dbtable, data) =>{
+    empty(data);
+    if(flag){
+        dbtable.bulkAdd([data]);
+        console.log("Data inserted successfully...")
+    }else{
+        console.log("Please Provide Data...")
+    }
+        return flag;
+}
+
+//CHECK TEXTBOX VALIDATION
+const empty = object =>{
+    let flag = false;
+
+    for(const value in object){
+        if(object[value]!="" && object.hasOwnProperty(value)){
+            flag = true;
+        }else{
+            flag = false;
+        }
+    }   return flag;
+}
+
 export default productdb;
+export {
+    bulkcreate;
+}
